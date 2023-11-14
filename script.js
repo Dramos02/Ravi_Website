799a4417ee793bd2d82066835a29ef6a1995359f
Kurt Colonia <kurtrobinperezcolonia@gmail.com>
2023-11-14 11:29:28 +0800

    15th Commit: Made home page responsive

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 // Dropdown JS Start [ kurut0 ]
 function dropDownFunc() {
-    var menu = document.getElementById("dropdown");
-    var dropBod = menu.querySelector(".navDropdown");
-    var dropOpt = menu.querySelectorAll(".navOptions");
-    var dropImg = menu.querySelectorAll(".navOptions img")
+  var headCont = document.querySelector(".headerContent");
+  var dropBod = document.querySelector(".navDropdown");
   
-    if(dropBod.style.maxHeight === "0px" || dropBod.style.maxHeight === "") {
-        dropBod.style.maxHeight = "1000px";
-        dropOpt.forEach(function(option) {
-            option.style.maxHeight = "1000px";
-        });
-        dropImg.forEach(function(image) {
-            image.style.maxHeight = "50px";
-        });
-    }
-    else {
-        dropImg.forEach(function(image) {
-            image.style.maxHeight = "0";
-        });
-        setTimeout(function() {
-            dropBod.style.maxHeight = "0";
-            dropOpt.forEach(function(option) {
-                option.style.maxHeight = "0";
-            });
-        }, 150);
-    }
+  if(dropBod.style.top === "10px") {
+    headCont.style.boxShadow = "1px 1px 10px #0F0F0F";
+    dropBod.style.top = "70px";
+  }
+  else {
+    headCont.style.boxShadow = "none";
+    dropBod.style.top = "10px";
+  }
 }
 // Dropdown JS End [ kurut0 ]
