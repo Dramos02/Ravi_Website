6102370d56b5e98c03564de9a75dc4a7b03b1700
Dominic Ramos <106921028+Dramos02@users.noreply.github.com>
2023-12-12 09:52:37 +0800

    Updated add.php

--- ravi(htdocs)FIREBASE-PHP/add.php
+++ ravi(htdocs)FIREBASE-PHP/add.php
@@ -17,7 +17,7 @@
     <tr>
         <td>Mobile No</td>
         <td>:</td>
-        <td><input type="text" name="contactnumber"  pattern="09\d{9}" title="Please enter 11 digits start with 09" placeholder="09XXXXXXX" required></td>
+        <td><input type="text" name="contactnumber"  class="input" pattern="09\d{9}" title="Please enter 11 digits start with 09" placeholder="09XXXXXXX" required></td>
         <td><input type="checkbox" id="projengineer_I" name="projengineer_I" value="projengineer_I">
             <label for="projengineer_I" class="checkbox-text">PROJ MANAGER</label>
         </td>
@@ -28,7 +28,7 @@
     <tr>
         <td>Address</td>
         <td>:</td>
-        <td><input type="text" name="address" placeholder="Complete Address"required></td>
+        <td><input type="text" name="address" class="input" placeholder="Complete Address"required></td>
         <td><input type="checkbox" id="architect_I" name="architect_I" value="architect_I">
             <label for="architect_I" class="checkbox-text">ARCHITECT</label>
         </td>
@@ -39,7 +39,7 @@
     <tr>
         <td>% ID Prof. Fee or Fixed PF Amount</td>
         <td>:</td>
-        <td><input type="text" name="idprofFee" placeholder=""required></td>
+        <td><input type="text" name="idprofFee" class="input" placeholder=""required></td>
         <td><input type="checkbox" id="others_I" name="others_I" value="others_I">
             <label for="others_I" class="checkbox-text">OTHERS: PLS SPECIFY</label>
         </td>
@@ -63,7 +63,7 @@
     <tr>
         <td>Mobile No</td>
         <td>:</td>
-        <td><input type="text" name="contactnumber"  pattern="09\d{9}" title="Please enter 11 digits start with 09" placeholder="09XXXXXXX" required></td>
+        <td><input type="text" name="contactnumber" class="input" pattern="09\d{9}" title="Please enter 11 digits start with 09" placeholder="09XXXXXXX" required></td>
         <td><input type="checkbox" id="projengineer_P" name="projengineer" value="projengineer_P">
             <label for="projengineer_P" class="checkbox-text">PROJ MANAGER</label>
         </td>
@@ -74,7 +74,7 @@
     <tr>
         <td>Address</td>
         <td>:</td>
-        <td><input type="text" name="address" placeholder="Complete Address"required></td>
+        <td><input type="text" name="address" class="input" placeholder="Complete Address"required></td>
         <td><input type="checkbox" id="architect_P" name="architect_P" value="architect_P">
             <label for="architect_P" class="checkbox-text">ARCHITECT</label>
         </td>
@@ -85,7 +85,7 @@
     <tr>
         <td>% ID Prof. Fee or Fixed PF Amount</td>
         <td>:</td>
-        <td><input type="text" name="idprofFee" placeholder=""required></td>
+        <td><input type="text" name="idprofFee" class="input" placeholder=""required></td>
         <td><input type="checkbox" id="others_P" name="others_P" value="others_P">
             <label for="others_P" class="checkbox-text">OTHERS: PLS SPECIFY</label>
         </td>
@@ -109,7 +109,7 @@
     <tr>
         <td>Mobile No</td>
         <td>:</td>
-        <td><input type="text" name="contactnumber"  pattern="09\d{9}" title="Please enter 11 digits start with 09" placeholder="09XXXXXXX" required></td>
+        <td><input type="text" name="contactnumber" class="input" pattern="09\d{9}" title="Please enter 11 digits start with 09" placeholder="09XXXXXXX" required></td>
         <td><input type="checkbox" id="projengineer_M" name="projengineer_M" value="projengineer_M">
             <label for="projengineer_M" class="checkbox-text">PROJ MANAGER</label>
         </td>
@@ -120,7 +120,7 @@
     <tr>
         <td>Address</td>
         <td>:</td>
-        <td><input type="text" name="address" placeholder="Complete Address"required></td>
+        <td><input type="text" name="address" class="input" placeholder="Complete Address"required></td>
         <td><input type="checkbox" id="architect_M" name="architect_M" value="architect_M">
             <label for="architect_M" class="checkbox-text">ARCHITECT</label>
         </td>
@@ -131,7 +131,7 @@
     <tr>
         <td>% ID Prof. Fee or Fixed PF Amount</td>
         <td>:</td>
-        <td><input type="text" name="idprofFee" placeholder=""required></td>
+        <td><input type="text" name="idprofFee" class="input" placeholder=""required></td>
         <td><input type="checkbox" id="others_M" name="others_M" value="others_M">
             <label for="others_M" class="checkbox-text">OTHERS: PLS SPECIFY</label>
         </td>
@@ -170,9 +170,128 @@
         <td>
         <textarea id="others_field_S" name="others_field_S" rows="1" cols="20"></textarea>
         </td>
+    </tr>   
     <tr>
-        <td><button type="submit" class="button-confirm">Submit</button></td>
-        
+        <td>Door Front</td>
+        <td><input type="text" name="doorfront" class="input" placeholder=""required></td>
+        <td>Door Front Finish</td>
+        <td><input type="text" name="doorfront_Finish" placeholder=""required></td>
+    </tr>
+    <tr>
+        <td>Counter Top</td>
+        <td><input type="text" name="countertop" class="input" placeholder=""required></td>
+        <td>Handle</td>
+        <td><input type="text" name="handle"  class="input" placeholder=""required></td>
+    </tr>
+    <tr>
+        <td>Own Type</td>
+        <td><input type="checkbox" id="built_in" name="built_in" value="built_in">
+            <label for="built_in" class="checkbox-text">BUILT IN</label>
+        </td>
+        <td><input type="checkbox" id="free_standing" name="free_standing" value="free_standing">
+            <label for="free_standing" class="checkbox-text">FREE STANDING</label>
+        </td>
+    </tr>
+    <tr>
+        <td>Oven</td>
+        <td><input type="text" name="width_Oven" class="input" placeholder=""required>
+        <label class="checkbox-text">WIDTH</label></td>
+        <td><input type="text" name="depth_Oven" class="input" placeholder=""required>
+        <label class="checkbox-text">DEPTH</label></td>
+        <td><input type="text" name="height_Oven" class="input" placeholder=""required>
+        <label class="checkbox-text">HEIGHT</label></td>
+    </tr>
+    <tr>
+        <td>Cooktop</td>
+        <td><input type="text" name="width_Cooktop" class="input" placeholder=""required>
+        <label class="checkbox-text">WIDTH</label></td>
+        <td><input type="text" name="depth_Cooktop" class="input" placeholder=""required>
+        <label class="checkbox-text">DEPTH</label></td>
+        <td><input type="text" name="height_Cooktop" class="input" placeholder=""required>
+        <label class="checkbox-text">HEIGHT</label></td>
+    </tr>
+    <tr>
+        <td>Range</td>
+        <td><input type="checkbox" id="built_in_Range" name="built_in_Range" value="built_in_Range">
+            <label for="built_in_Range" class="checkbox-text">BUILT IN</label>
+        </td>
+        <td><input type="checkbox" id="free_standing_Range" name="free_standing_Range" value="free_standing_Range">
+            <label for="free_standing_Range" class="checkbox-text">FREE STANDING</label>
+        </td>
+    </tr>
+    <tr>
+        <td>RangeHood</td>
+        <td><input type="checkbox" id="duct_type" name="duct_type" value="duct_type">
+            <label for="duct_type" class="checkbox-text">DUCT TYPE</label>
+        </td>
+        <td><input type="checkbox" id="filter_type" name="filter_type" value="filter_type">
+            <label for="filter_type" class="checkbox-text">FILTER TYPE</label>
+        </td>
+        <td><input type="checkbox" id="nine_9m" name="nine_9m" value="nine_9m">
+            <label for="nine_9m" class="checkbox-text">900mm</label>
+        </td>
+        <td><input type="checkbox" id="six_6m" name="six_6m" value="six_6m">
+            <label for="six_6m" class="checkbox-text">600mm</label>
+        </td>
+    </tr>
+    <tr>
+        <td>Refrigerator</td>
+        <td><input type="text" name="width_Refrigerator" class="input" placeholder=""required>
+        <label class="checkbox-text">WIDTH</label></td>
+        <td><input type="text" name="depth_Refrigerator" class="input" placeholder=""required>
+        <label class="checkbox-text">DEPTH</label></td>
+        <td><input type="text" name="height_Refrigerator" class="input" placeholder=""required>
+        <label class="checkbox-text">HEIGHT</label></td>
+    </tr>
+    <tr>
+        <td>Sink</td>
+        <td><input type="text" name="width_Sink" class="input" placeholder=""required>
+        <label class="checkbox-text">WIDTH</label></td>
+        <td><input type="text" name="depth_Sink" class="input" placeholder=""required>
+        <label class="checkbox-text">DEPTH</label></td>
+        <td><input type="text" name="height_Sink" class="input" placeholder=""required>
+        <label class="checkbox-text">HEIGHT</label></td>
+    </tr>
+    <tr>
+        <td>Microwave</td>
+        <td><input type="text" name="width_Microwave" class="input" placeholder=""required>
+        <label class="checkbox-text">WIDTH</label></td>
+        <td><input type="text" name="depth_Microwave" class="input" placeholder=""required>
+        <label class="checkbox-text">DEPTH</label></td>
+        <td><input type="text" name="height_Microwave" class="input" placeholder=""required>
+        <label class="checkbox-text">HEIGHT</label></td>
+    </tr>
+    <tr>
+        <td>Model/Brand (OTHER APPLIANCES, PLS SPECIFY)</td>
+        <td><input type="text" name="model_brand" class="input" placeholder=""required>
+    </tr>
+    <tr>
+    <td></td>
+    <td><input type="text" name="width_model_brand" class="input" placeholder=""required>
+        <label class="checkbox-text">WIDTH</label></td>
+        <td><input type="text" name="depth_model_brand" class="input" placeholder=""required>
+        <label class="checkbox-text">DEPTH</label></td>
+        <td><input type="text" name="height_model_brand" class="input" placeholder=""required>
+        <label class="checkbox-text">HEIGHT</label></td>
+    </tr>
+    <tr>
+        <td>REMARKS:</td>
+        <td><textarea id="remarks" name="remarks" rows="3" cols="50"></textarea></td>
+    </tr>
+    <tr>
+    <td>Attachments:</td>
+    <td><label>Plan</label>
+        <input type="file" id="plan" name="plan"></td>
+    <td><label for="picture_loc_ceiling">Picture of location and ceilling if with cover</label><input type="file" id="plan" name="plan"></td>
+    </tr>
+    <tr>
+        <td>Submitted by:</td>
+        <td><input type="text" name="submittedby" class="input" placeholder=""required></td>
+        <td><label for="Date">Date:</label>
+            <input type="date" id="birthday" name="birthday"></td>
+    </tr>
+    <tr>  
+        <td><button type="submit" class="button-confirm">Submit</button></td> 
     </tr>
 </form>
 </table>
@@ -230,7 +349,7 @@ input[type="checkbox"] {
     transform: scale(1.5); /* Adjust the scale factor as needed */
 }
 
-.checkbox-text {
+label {
     font-size: 14px;
     font-weight: bold;
 }
